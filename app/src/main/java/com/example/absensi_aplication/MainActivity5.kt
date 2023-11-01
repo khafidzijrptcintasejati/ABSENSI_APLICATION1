4bab8a12c7902f6066b29594f077460b1621db6b
sariefita <sariefita081@gmail.com>
2023-10-31 18:27:30 -0700

    MENGCODING MAIN ACTIVITY 5

--- app/src/main/java/com/example/absensi_aplication/MainActivity5.kt
+++ app/src/main/java/com/example/absensi_aplication/MainActivity5.kt
@@ -1,11 +1,23 @@
 package com.example.absensi_aplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.absensi_aplication.databinding.ActivityMain5Binding
 
 class MainActivity5 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain5Binding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMain5Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main5)
+        setContentView(binding.root)
+
+        binding.btnguru2.setOnClickListener{
+            startActivity(Intent(this,MainActivity4::class.java))
+        }
+        binding.btnmrd2.setOnClickListener{
+            startActivity(Intent(this,MainActivity4::class.java))
+        }
+
     }
 }

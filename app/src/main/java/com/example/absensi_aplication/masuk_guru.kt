b04dbc8aa326b96be806a00ebaef7b25c94c38a4
sariefita <sariefita081@gmail.com>
2023-10-31 21:15:57 -0700

    menambahkan input guru

---
+++ app/src/main/java/com/example/absensi_aplication/masuk_guru.kt
@@ -0,0 +1,37 @@
+package com.example.absensi_aplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.absensi_aplication.databinding.ActivityMasukGuruBinding
+import com.example.absensi_aplication.room.DATABASE
+
+class masuk_guru : AppCompatActivity() {
+    private lateinit var binding :ActivityMasukGuruBinding
+    private val db by lazy { DATABASE.getInstance(this) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMasukGuruBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val nama = binding.namaguru
+        val nis = binding.nipguru
+        binding.masukguru.setOnClickListener{
+            if (nama.text.isNotEmpty()&&nis.text.isNotEmpty()) {
+                startActivity(
+                    Intent(this, tampilguru::class.java).putExtra("nama", nama.text.toString())
+                        .putExtra("nis", nis.text.toString())
+                )
+                alert("Login berhasil")
+
+            }else{
+                alert("Isi data terlebih dahulu")
+            }
+        }
+    }
+
+    private fun alert(msg:String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+}
